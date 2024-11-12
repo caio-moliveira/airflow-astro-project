85a89cd7721b17f167d26d9e880588f75e55b270
Caio Machado <moliveiracaio@gmail.com>
2024-11-12 14:42:09 -0300

    updating

--- include/db.py
+++ include/db.py
@@ -6,7 +6,17 @@ import os
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = os.getenv("URLPOSTGRES")
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# PostgreSQL configuration
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+
+# Create a SQLAlchemy connection string
+db_url = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+# Initialize database engine and session
+engine = create_engine(db_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
